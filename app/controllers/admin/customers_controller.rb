a86450d070756f1a831543796c3d799ee8b65055
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2022-11-26 13:39:26 +0000

    Update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,12 @@
 class Admin::CustomersController < ApplicationController
+ def index
+   @items = Item.all
+   @customers = Customer.all
+   @customers = Customer.page(params[:page]).per(10)
+ end
+
+ def show
+   @customer = Customer.find(params[:id])
+ end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,5 @@
 class Customer < ApplicationRecord
   # has_many :addresses, :orders, :cart_items, dependent: :destroy
-  
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
