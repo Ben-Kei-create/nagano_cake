3b425e174a570055ceede05706b2b40ef628f17b
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-01-09 11:01:16 +0000

    Finish

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -10,11 +10,12 @@ class Public::CartItemsController < ApplicationController
  end
 
  def create
-  @cart_item = CartItem.new(cart_item_params)
-		@cart_item.customer_id = current_customer.id
+   @cart_item = CartItem.new(cart_item_params)
+		 @cart_item.customer_id = current_customer.id
 		if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-			#current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).number += params[:cart_item][:number].to_i
-			current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).save
+		 @already = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+	  @already.amount += params[:cart_item][:amount].to_i
+			@already.save
 			redirect_to cart_items_path
 		elsif @cart_item.save
 			redirect_to cart_items_path
