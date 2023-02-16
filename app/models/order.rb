c45b0677a7b569f59a68bd6600de26b9bc6b45dd
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-02-16 11:28:51 +0000

    Customerの購入履歴修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :customer
+  has_many :items, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum is_active: { waiting_for_payment: 0, payment_confirmation: 1, in_production: 2, preparing_to_ship: 3, sent: 4 }
